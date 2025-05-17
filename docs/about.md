2f7a1966143b239fd1437acd6836d667cc146e94
蛟龙 <1610105206@qq.com>
2025-05-17 22:09:49 +0800

    Intial Commit

---
+++ docs/about.md
@@ -0,0 +1,12 @@
+---
+title: 关于
+sidebar_position: 1
+---
+
+# 介绍
+
+基于Taboolib开发的一个多功能插件。
+
+## 功能
+
++ 物品引擎
---
+++ docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+
+  customFields: {
+    // 标题前缀
+    titlePrefix: "主页",
+    // 开始按钮文字
+    start: "快速开始 🥵",
+  },
+
+  // 标题部分
+  title: "Ratziel",
+  titleDelimiter: "|",
+  // 描述信息
+  tagline: "插件文档",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://theflooddragon.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/Ratziel-Beta/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "Altawk/TheFloodDragon", // Usually your GitHub org/user name.
+  projectName: "Ratziel-Wiki", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans", "en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/TheFloodDragon/Ratziel-Beta/tree/master/wiki/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Ratziel",
+        logo: {
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "about",
+            position: "left",
+            label: "文档",
+          },
+          // 搜索框
+          {
+            type: "search",
+            position: "right",
+          },
+          // Github
+          {
+            href: "https://github.com/TheFloodDragon/Ratziel-Beta",
+            className: "header-github-link",
+            position: "right",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
+          },
+        ],
+      },
+      // 底部信息
+      footer: {
+        style: "dark",
+        // 底部版权信息
+        copyright: `Copyright © ${new Date().getFullYear()} <b>TheFloodDragon</b>, All Rights Reserved.`,
+      },
+      // 深浅主题
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        language: ["en", "zh"],
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+        indexBlog: false,
+        docsRouteBasePath: "/",
+      },
+    ],
+  ],
+};
+
+export default config;
