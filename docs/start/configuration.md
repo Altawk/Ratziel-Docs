5ac4dd97b624b715a463495e792c2c6e208656aa
蛟龙 <75253383+TheFloodDragon@users.noreply.github.com>
2025-06-08 09:24:32 +0000

    2

--- docs/start/configuration.md
+++ docs/start/configuration.md
@@ -18,4 +18,42 @@ Ratziel
 
 打开主要配置文件 `settings.yml` 后，你将看到如下内容：
 
-TODO
+```YAML
+Workspaces:
+  # 默认工作空间
+  default:
+    # 若开启则会监听工作空间内的文件变化,并自动重新加载变化文件
+    listen: true
+    filter: '^(?![#!]).*\.(?i)(yaml|yml|toml|tml|json|conf)$'
+    path: 'plugins/Ratziel/workspace'
+    # 是否使用文件名称当做元素名称
+    use-filename: false
+    # 统一元素类型 (None 代表不统一)
+    unified-type: None
+
+Message:
+  # 是否在消息组件序列化的过程中默认取消斜体
+  non-italic-by-default: true
+
+Script:
+  # 默认使用的脚本语言
+  default: JavaScript
+  languages:
+    # JavaScript 脚本语言
+    JavaScript:
+      # 是否启用
+      enabled: true
+      # 使用的脚本引擎 (Nashorn/GraalJS)
+      engine: nashorn
+    # Kether (暂不支持)
+    Kether:
+      enabled: false
+      # 是否启用宽容解析
+      tolerance: true
+    # Jexl
+    Jexl:
+      enabled: true
+    # Kotlin Scripting (暂不支持)
+    KotlinScripting:
+      enabled: false
+```
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,27 +4,27 @@ import styles from "./styles.module.css";
 
 const FeatureList = [
     {
-        title: "强大的物品系统",
+        title: "Efficient",
         Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
         description: <>内置丰富的物品属性配置，支持模板继承、动态数据和交互触发器</>,
     },
     {
-        title: "多语言脚本引擎",
+        title: "Powerful",
         Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-        description: <>支持JavaScript、Kether、Kotlin脚本等多种脚本语言，灵活实现复杂功能</>,
+        description: <>多脚本语言支持，灵活实现复杂功能</>,
     },
     {
-        title: "完善的插件兼容",
+        title: "Expandability",
         Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
         description: <>与常见物品插件如AzureFlow、NeigeItems等良好兼容，易于集成现有系统</>,
     },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
     return (
         <div className={clsx("col col--4")}>
             <div className="text--center">
-                <Svg className={styles.featureSvg} role="img"/>
+                <Svg className={styles.featureSvg} role="img" />
             </div>
             <div className="text--center padding-horiz--md">
                 <Heading as="h3">{title}</Heading>
